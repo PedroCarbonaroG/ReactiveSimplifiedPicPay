90c8f78bc119f9940a027cb725d0dedf5cf837a7
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-05-25 23:37:59 -0300

    feature: Api new structure for responseCodes

--- src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/api/IPersonAPI.java
+++ src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/api/IPersonAPI.java
@@ -1,8 +1,12 @@
 package com.carbonaro.ReactiveSimplifiedPicPay.api;
 
+import com.carbonaro.ReactiveSimplifiedPicPay.core.exceptionHandler.response.ErrorEmptyResponse;
+import com.carbonaro.ReactiveSimplifiedPicPay.core.exceptionHandler.response.ErrorResponse;
 import com.carbonaro.ReactiveSimplifiedPicPay.domain.responses.LegalPersonResponse;
 import com.carbonaro.ReactiveSimplifiedPicPay.domain.responses.NaturalPersonResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
@@ -17,10 +21,25 @@ public interface IPersonAPI {
     @Operation(
             summary = "Responsible route to return all LegalPersons in DataBase.",
             responses = {
-                    @ApiResponse(responseCode = "200", description = "OK"),
-                    @ApiResponse(responseCode = "204", description = "Neither LegalPerson was found."),
-                    @ApiResponse(responseCode = "404", description = "Not Found. Resources are not found to complete the search for all LegalPersons."),
-                    @ApiResponse(responseCode = "500", description = "Internal Server Error. Something went wrong with API, contact the administration.")
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = LegalPersonResponse.class))),
+
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Neither LegalPerson was found.",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorEmptyResponse.class))),
+
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not Found. Resources are not found to complete the search for all LegalPersons.",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorResponse.class))),
+
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal Server Error. Something went wrong with API, contact the administration.",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorResponse.class)))
             })
     @GetMapping(value = "/legals")
     Flux<LegalPersonResponse> findAllLegals();
@@ -28,10 +47,25 @@ public interface IPersonAPI {
     @Operation(
             summary = "Responsible route to return all NaturalPersons in DataBase.",
             responses = {
-                    @ApiResponse(responseCode = "200", description = "OK"),
-                    @ApiResponse(responseCode = "204", description = "Neither NaturalPerson was found."),
-                    @ApiResponse(responseCode = "404", description = "Not Found. Resources are not found to complete the search for all NaturalPersons."),
-                    @ApiResponse(responseCode = "500", description = "Internal Server Error. Something went wrong with API, contact the administration."),})
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = NaturalPersonResponse.class))),
+
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Neither NaturalPerson was found.",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorEmptyResponse.class))),
+
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not Found. Resources are not found to complete the search for all NaturalPersons.",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorResponse.class))),
+
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal Server Error. Something went wrong with API, contact the administration.",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorResponse.class))),})
     @GetMapping(value = "/naturals")
     Flux<NaturalPersonResponse> findAllNaturals();
 }
