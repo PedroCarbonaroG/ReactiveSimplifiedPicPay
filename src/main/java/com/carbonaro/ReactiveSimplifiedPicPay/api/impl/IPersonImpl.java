2a397bfda8419d7244e534404fea7b272f2d40fa
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-05-25 23:38:18 -0300

    refactor: ApiImpl

--- src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/api/impl/IPersonImpl.java
+++ src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/api/impl/IPersonImpl.java
@@ -5,18 +5,16 @@ import com.carbonaro.ReactiveSimplifiedPicPay.domain.responses.LegalPersonRespon
 import com.carbonaro.ReactiveSimplifiedPicPay.domain.responses.NaturalPersonResponse;
 import com.carbonaro.ReactiveSimplifiedPicPay.services.LegalPersonService;
 import com.carbonaro.ReactiveSimplifiedPicPay.services.NaturalPersonService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
-@RequiredArgsConstructor
-@EnableReactiveMongoRepositories
+@AllArgsConstructor
 public class IPersonImpl implements IPersonAPI {
 
-    private NaturalPersonService naturalPersonService;
-    private LegalPersonService legalPersonService;
+    private final NaturalPersonService naturalPersonService;
+    private final LegalPersonService legalPersonService;
 
     @Override
     public Flux<LegalPersonResponse> findAllLegals() {
