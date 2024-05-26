975b927f096436efa37bc5ae982875b65f696d12
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-05-26 15:35:56 -0300

    feature: included EnableWebFlux and EnableReactiveMongoRepositories dependencies to source class

--- src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/ReactiveSimplifiedPicPayApplication.java
+++ src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/ReactiveSimplifiedPicPayApplication.java
@@ -2,8 +2,12 @@ package com.carbonaro.ReactiveSimplifiedPicPay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
+@EnableWebFlux
 @SpringBootApplication
+@EnableReactiveMongoRepositories
 public class ReactiveSimplifiedPicPayApplication {
 
 	public static void main(String[] args) {
