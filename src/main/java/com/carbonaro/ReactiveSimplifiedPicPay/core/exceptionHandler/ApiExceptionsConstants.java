8ff2bdb04677b279bd1c6b90e0f57ab96c3fbbcb
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-05-25 23:40:51 -0300

    feature: ApiExceptionHandlerConstants for assist the handler

---
+++ src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/core/exceptionHandler/ApiExceptionsConstants.java
@@ -0,0 +1,14 @@
+package com.carbonaro.ReactiveSimplifiedPicPay.core.exceptionHandler;
+
+public abstract class ApiExceptionsConstants {
+
+    protected static final String NO_CONTENT = "No Content";
+    protected static final String NOT_FOUND = "Not Found";
+    protected static final String INTERNAL_SERVER_ERROR = "Internal Server error";
+
+    // <=====================================================================================================================================================> //
+
+    protected static final String INTERNAL_SERVER_ERROR_MESSAGE = "Internal Server Error, contact the administration for solve the problem!";
+    protected static final String NO_CONTENT_WARNING_MESSAGE = "No Content, the search was done with success but non Object(s) was found!";
+    protected static final String NOT_FOUND_ERROR_MESSAGE = "Not Found, some resources are not available";
+}
