ddcb30b5feee7edb5e26355e16cb505b5d182198
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-05-27 08:29:01 -0300

    feature: LegalPersonRepository

--- src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/repositories/LegalPersonRepository.java
+++ src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/repositories/LegalPersonRepository.java
@@ -1,9 +1,14 @@
 package com.carbonaro.ReactiveSimplifiedPicPay.repositories;
 
 import com.carbonaro.ReactiveSimplifiedPicPay.domain.entities.LegalPerson;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface LegalPersonRepository extends ReactiveMongoRepository<LegalPerson, String> {
+
+    @Schema(description = "Retornar um LegalPerson por meio do seu CNPJ.")
+    Mono<LegalPerson> findByCnpj(String cnpj);
 }
