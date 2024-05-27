4fb3b64e813e80e90c457328601ab768e40ddfaa
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-05-27 08:29:09 -0300

    feature: NaturalPersonRepository

--- src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/repositories/NaturalPersonRepository.java
+++ src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/repositories/NaturalPersonRepository.java
@@ -1,9 +1,17 @@
 package com.carbonaro.ReactiveSimplifiedPicPay.repositories;
 
 import com.carbonaro.ReactiveSimplifiedPicPay.domain.entities.NaturalPerson;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface NaturalPersonRepository extends ReactiveMongoRepository<NaturalPerson, String> {
+
+    @Schema(description = "Retornar um NaturalPerson por meio do seu nome completo.")
+    Mono<NaturalPerson> findByName(String name);
+
+    @Schema(description = "Retornar um NaturalPerson por meio do seu CPF.")
+    Mono<NaturalPerson> findByCpf(String cpf);
 }
