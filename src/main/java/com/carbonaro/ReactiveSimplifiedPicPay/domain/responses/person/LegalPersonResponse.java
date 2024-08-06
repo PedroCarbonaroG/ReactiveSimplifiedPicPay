84ef3b3cbd0f3fde90e6e67a216650b6b82688fb
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-08-06 19:05:18 -0300

    Feature | LegalPersonResponse created

---
+++ src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/domain/responses/person/LegalPersonResponse.java
@@ -0,0 +1,26 @@
+package com.carbonaro.ReactiveSimplifiedPicPay.domain.responses.person;
+
+import com.carbonaro.ReactiveSimplifiedPicPay.domain.enums.CompanySizeEnum;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LegalPersonResponse extends PersonResponse {
+
+    @Schema(description = "Demonstration of the unique LegalPerson CNPJ.")
+    private String cnpj;
+
+    @Schema(description = "Demonstration of the company size")
+    private CompanySizeEnum companySize;
+
+    @Schema(description = "Demonstration of the company partners")
+    List<NaturalPersonResponse> partners;
+}
+
