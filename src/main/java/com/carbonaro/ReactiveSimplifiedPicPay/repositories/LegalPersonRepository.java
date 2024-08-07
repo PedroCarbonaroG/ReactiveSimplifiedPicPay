9ac7f631551de3cb819b4c28ddb53644654cb40d
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-08-07 10:34:40 -0300

    Refactor | Description instead schema

--- src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/repositories/LegalPersonRepository.java
+++ src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/repositories/LegalPersonRepository.java
@@ -1,7 +1,7 @@
 package com.carbonaro.ReactiveSimplifiedPicPay.repositories;
 
 import com.carbonaro.ReactiveSimplifiedPicPay.domain.entities.LegalPerson;
-import io.swagger.v3.oas.annotations.media.Schema;
+import jdk.jfr.Description;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
@@ -9,6 +9,6 @@ import reactor.core.publisher.Mono;
 @Repository
 public interface LegalPersonRepository extends ReactiveMongoRepository<LegalPerson, String> {
 
-    @Schema(description = "Retornar um LegalPerson por meio do seu CNPJ.")
+    @Description("Returns a LegalPerson by it's unique CNPJ.")
     Mono<LegalPerson> findByCnpj(String cnpj);
 }
