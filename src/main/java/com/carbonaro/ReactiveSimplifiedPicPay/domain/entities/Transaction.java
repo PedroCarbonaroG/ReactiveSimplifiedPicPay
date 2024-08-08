adc16ce2339591bf33c9403da4b6884745306b7d
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-08-08 17:55:45 -0300

    Feature | Transaction entity

---
+++ src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/domain/entities/Transaction.java
@@ -0,0 +1,30 @@
+package com.carbonaro.ReactiveSimplifiedPicPay.domain.entities;
+
+import jdk.jfr.Description;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(value = "Transaction")
+public class Transaction {
+
+    @Id
+    @Description("Transaction Identifier.")
+    private String id;
+
+    @Description("Identifier from the transaction sender.")
+    private String senderDocument;
+
+    @Description("Identifier from the transaction receiver.")
+    private String receiverDocument;
+
+    @Description("Total transaction amount.")
+    private BigDecimal transactionValue;
+}
