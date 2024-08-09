b5f434a5e4cc9cf55e3320fd501c93ad17e6e6fb
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-08-09 12:30:41 -0300

    Refactor | ApiExceptionHandler adjustments

--- src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/core/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/core/exceptionHandler/ApiExceptionHandler.java
@@ -2,9 +2,7 @@ package com.carbonaro.ReactiveSimplifiedPicPay.core.exceptionHandler;
 
 import com.carbonaro.ReactiveSimplifiedPicPay.core.exceptionHandler.response.ErrorEmptyResponse;
 import com.carbonaro.ReactiveSimplifiedPicPay.core.exceptionHandler.response.ErrorResponse;
-import com.carbonaro.ReactiveSimplifiedPicPay.services.exceptions.EmptyException;
-import com.carbonaro.ReactiveSimplifiedPicPay.services.exceptions.EmptyOrNullObjectException;
-import com.carbonaro.ReactiveSimplifiedPicPay.services.exceptions.NotFoundException;
+import com.carbonaro.ReactiveSimplifiedPicPay.services.exceptions.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -25,9 +23,9 @@ public class ApiExceptionHandler extends ApiExceptionsConstants {
     private void getPrivateStackTrace(Exception e) {
 
         if (e instanceof EmptyException) {
-            log.warn("ERROR        ====> {}", e.getMessage());
-            log.warn("ERROR CLASS  ====> {}", e.getClass());
-            log.warn("STACKTRACE   ====> {}", (Object) e.getStackTrace());
+            log.warn("WARN           ====> {}", e.getMessage());
+            log.warn("WARNING CLASS  ====> {}", e.getClass());
+            log.warn("STACKTRACE     ====> {}", (Object) e.getStackTrace());
         } else {
             log.error("ERROR        ====> {}", e.getMessage());
             log.error("ERROR CLASS  ====> {}", e.getClass());
@@ -41,7 +39,7 @@ public class ApiExceptionHandler extends ApiExceptionsConstants {
         getPrivateStackTrace(e);
         ErrorEmptyResponse response = ErrorEmptyResponse.builder()
                 .error(NO_CONTENT)
-                .timestemp(TIMESTEMP)
+                .timestamp(TIMESTEMP)
                 .path(getPath(request))
                 .status(NO_CONTENT_STATUS.value())
                 .warningMessage(NO_CONTENT_WARNING_MESSAGE)
@@ -51,13 +49,13 @@ public class ApiExceptionHandler extends ApiExceptionsConstants {
     }
 
     @ExceptionHandler({MethodArgumentNotValidException.class, MissingRequestValueException.class, ServerWebInputException.class,
-    EmptyOrNullObjectException.class, DataIntegrityViolationException.class})
+    EmptyOrNullObjectException.class, DataIntegrityViolationException.class, TransactionValidationException.class, BadRequestException.class})
     private ResponseEntity<ErrorResponse> badRequestExceptionHandler(Exception e, ServerWebExchange request) {
 
         getPrivateStackTrace(e);
         ErrorResponse response = ErrorResponse.builder()
                 .error(BAD_REQUEST)
-                .timestemp(TIMESTEMP)
+                .timestamp(TIMESTEMP)
                 .path(getPath(request))
                 .status(BAD_REQUEST_STATUS.value())
                 .errorMessage(BAD_REQUEST_ERROR_MESSAGE)
@@ -72,7 +70,7 @@ public class ApiExceptionHandler extends ApiExceptionsConstants {
         getPrivateStackTrace(e);
         ErrorResponse response = ErrorResponse.builder()
                 .error(NOT_FOUND)
-                .timestemp(TIMESTEMP)
+                .timestamp(TIMESTEMP)
                 .path(getPath(request))
                 .status(NOT_FOUND_STATUS.value())
                 .errorMessage(NOT_FOUND_ERROR_MESSAGE)
@@ -86,7 +84,7 @@ public class ApiExceptionHandler extends ApiExceptionsConstants {
 
         getPrivateStackTrace(e);
         ErrorResponse response = ErrorResponse.builder()
-                .timestemp(TIMESTEMP)
+                .timestamp(TIMESTEMP)
                 .path(getPath(request))
                 .error(INTERNAL_SERVER_ERROR)
                 .status(INTERNAL_SERVER_STATUS.value())
