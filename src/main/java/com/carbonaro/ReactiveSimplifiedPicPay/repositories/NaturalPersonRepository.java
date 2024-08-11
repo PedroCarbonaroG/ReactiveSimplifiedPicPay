7e977298ea2386a6fa706e0914afbcff2aac5810
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-08-11 14:58:57 -0300

    Refactor | NaturalPersonRepository to english version

--- src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/repositories/NaturalPersonRepository.java
+++ src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/repositories/NaturalPersonRepository.java
@@ -1,7 +1,7 @@
 package com.carbonaro.ReactiveSimplifiedPicPay.repositories;
 
 import com.carbonaro.ReactiveSimplifiedPicPay.domain.entities.NaturalPerson;
-import io.swagger.v3.oas.annotations.media.Schema;
+import jdk.jfr.Description;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
@@ -9,9 +9,9 @@ import reactor.core.publisher.Mono;
 @Repository
 public interface NaturalPersonRepository extends ReactiveMongoRepository<NaturalPerson, String> {
 
-    @Schema(description = "Retornar um NaturalPerson por meio do seu nome completo.")
-    Mono<NaturalPerson> findByName(String name);
-
-    @Schema(description = "Retornar um NaturalPerson por meio do seu CPF.")
+    @Description("Returns a NaturalPerson by it's unique CPF")
     Mono<NaturalPerson> findByCpf(String cpf);
+
+    @Description("Successfully delete an NaturalPerson by it's CPF")
+    Mono<Void> deleteByCpf(String cpf);
 }
