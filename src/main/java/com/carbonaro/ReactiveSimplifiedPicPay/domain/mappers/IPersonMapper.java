176b39af1109f171053ce279ca094ca2eb2af2c5
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-08-12 19:35:53 -0300

    Feature | IPersonMapper created for mapping DTOs

--- src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/domain/mappers/IPersonMapper.java
+++ src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/domain/mappers/IPersonMapper.java
@@ -2,8 +2,11 @@ package com.carbonaro.ReactiveSimplifiedPicPay.domain.mappers;
 
 import com.carbonaro.ReactiveSimplifiedPicPay.domain.entities.LegalPerson;
 import com.carbonaro.ReactiveSimplifiedPicPay.domain.entities.NaturalPerson;
-import com.carbonaro.ReactiveSimplifiedPicPay.domain.responses.LegalPersonResponse;
-import com.carbonaro.ReactiveSimplifiedPicPay.domain.responses.NaturalPersonResponse;
+import com.carbonaro.ReactiveSimplifiedPicPay.domain.requests.LegalPersonRequest;
+import com.carbonaro.ReactiveSimplifiedPicPay.domain.requests.NaturalPersonRequest;
+import com.carbonaro.ReactiveSimplifiedPicPay.domain.responses.person.LegalPersonResponse;
+import com.carbonaro.ReactiveSimplifiedPicPay.domain.responses.person.NaturalPersonResponse;
+import com.carbonaro.ReactiveSimplifiedPicPay.domain.responses.transaction.LegalPersonTransactionResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -12,7 +15,10 @@ public interface IPersonMapper {
 
     IPersonMapper INSTANCE = Mappers.getMapper(IPersonMapper.class);
 
-    LegalPersonResponse toLegalPersonResponse(LegalPerson person);
+    LegalPersonResponse toLegalPersonResponse(LegalPerson legalPerson);
+    NaturalPersonResponse toNaturalPersonResponse(NaturalPerson naturalPerson);
+    LegalPersonTransactionResponse toLegalPersonTransactionResponse(LegalPerson legalPerson);
+    LegalPerson toLegalPersonByRequest(LegalPersonRequest legalPersonRequest);
+    NaturalPerson toNaturalPersonByRequest(NaturalPersonRequest naturalPersonRequest);
 
-    NaturalPersonResponse toNaturalPersonResponse(NaturalPerson person);
 }
