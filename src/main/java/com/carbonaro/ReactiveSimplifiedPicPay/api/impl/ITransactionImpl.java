fd7cecba882f6e9859027b65d9e2ef06d1d6dfb8
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-08-15 12:34:49 -0300

    Feature | ITransactionImpl created with basic methods

---
+++ src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/api/impl/ITransactionImpl.java
@@ -0,0 +1,38 @@
+package com.carbonaro.ReactiveSimplifiedPicPay.api.impl;
+
+import com.carbonaro.ReactiveSimplifiedPicPay.api.ITransactionAPI;
+import com.carbonaro.ReactiveSimplifiedPicPay.domain.mappers.ITransactionMapper;
+import com.carbonaro.ReactiveSimplifiedPicPay.domain.requests.TransactionRequest;
+import com.carbonaro.ReactiveSimplifiedPicPay.domain.responses.transaction.TransactionResponse;
+import com.carbonaro.ReactiveSimplifiedPicPay.services.TransactionService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@AllArgsConstructor
+public class ITransactionImpl implements ITransactionAPI {
+
+    private TransactionService transactionService;
+    private ITransactionMapper transactionMapper;
+
+    @Override
+    public Flux<TransactionResponse> findAllTransactions() {
+
+        return transactionService
+                .findAllTransactions()
+                .flatMap(transactionMapper::toTransactionResponse);
+    }
+
+    @Override
+    public Mono<Void> saveTransaction(TransactionRequest transaction) {
+
+        return Mono
+                .just(transaction)
+                .map(transactionMapper::toTransactionByRequest)
+                .flatMap(transactionService::saveTransaction);
+    }
+
+
+}
