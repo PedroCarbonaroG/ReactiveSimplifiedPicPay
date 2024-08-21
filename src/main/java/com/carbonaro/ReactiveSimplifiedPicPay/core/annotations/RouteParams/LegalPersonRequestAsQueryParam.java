0f00da8bc136ccd6120a626ea8710ca57c5e873b
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-08-20 21:21:36 -0300

    Feature | LegalPersonRequestAsQueryParam creation

---
+++ src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/core/annotations/RouteParams/LegalPersonRequestAsQueryParam.java
@@ -0,0 +1,44 @@
+package com.carbonaro.ReactiveSimplifiedPicPay.core.annotations.RouteParams;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Parameter(
+        in = ParameterIn.QUERY,
+        name = "employeesNumber",
+        description = "Number of enterprise employees.",
+        schema = @Schema(type = "Integer"),
+        required = true)
+@Parameter(
+        in = ParameterIn.QUERY,
+        name = "name",
+        description = "Enterprise full name.",
+        schema = @Schema(type = "String"),
+        required = true)
+@Parameter(
+        in = ParameterIn.QUERY,
+        name = "email",
+        description = "Enterprise full email.",
+        schema = @Schema(type = "String"),
+        required = true)
+@Parameter(
+        in = ParameterIn.QUERY,
+        name = "address",
+        description = "Enterprise full address.",
+        schema = @Schema(type = "String"),
+        required = true)
+@Parameter(
+        in = ParameterIn.QUERY,
+        name = "password",
+        description = "Enterprise full and secret password.",
+        schema = @Schema(type = "String"),
+        required = true)
+public @interface LegalPersonRequestAsQueryParam {
+}
