bb284eb7fe30680b54c4f3ea30c53f62e89a201a
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-09-01 19:16:43 -0300

    Feature | MessageHelper for easier way to treat exception messages to user

--- src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/core/helper/MessageHelper.java
+++ src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/core/helper/MessageHelper.java
@@ -5,32 +5,34 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.MessageSourceResolvable;
 import org.springframework.context.NoSuchMessageException;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContextHolder;
 import java.util.Locale;
 
 @Configuration
 @RequiredArgsConstructor
 public class MessageHelper implements MessageSource {
 
-    public String getMessage(String message) {
+    private final MessageSource messageSource;
 
-        return getMessage(message, new Object[0], Locale.getDefault());
+    public String getMessage(String code) {
+
+        return messageSource.getMessage(code, new Object[0], LocaleContextHolder.getLocale());
     }
 
     @Override
     public String getMessage(String code, Object[] args, String defaultMessage, Locale locale) {
-
-        return "Not Implemented";
+        return messageSource.getMessage(code, args, defaultMessage, locale);
     }
 
     @Override
     public String getMessage(String code, Object[] args, Locale locale) throws NoSuchMessageException {
 
-        return "Not Implemented";
+        return messageSource.getMessage(code, args, locale);
     }
 
     @Override
     public String getMessage(MessageSourceResolvable resolvable, Locale locale) throws NoSuchMessageException {
 
-        return "Not Implemented";
+        return messageSource.getMessage(resolvable, locale);
     }
 }
