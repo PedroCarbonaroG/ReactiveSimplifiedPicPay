b9c57f090a71271e923e08fb8bf9a64753ce1e42
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-09-01 19:20:24 -0300

    Feature | X-API-Token for all routes do delegate security matters

--- src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/core/configs/SwaggerConfig.java
+++ src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/core/configs/SwaggerConfig.java
@@ -1,8 +1,15 @@
 package com.carbonaro.ReactiveSimplifiedPicPay.core.configs;
 
+import static io.swagger.v3.oas.annotations.enums.ParameterIn.HEADER;
+import static java.lang.Boolean.TRUE;
+import static java.util.Objects.nonNull;
+
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.PathItem;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.info.Info;
@@ -15,6 +22,10 @@ public class SwaggerConfig {
     private static final String DEFAULT_API_VERSION = "1.0.0";
     private static final String DEFAULT_API_DESCRIPTION = "A web service using reactive programming to solve PicPay challenge";
 
+    private static final String API_TOKEN = "AUTH-API-TOKEN";
+    private static final String USER_AUTHORIZATION = "Authorization (USER LEVEL NEEDED)";
+    private static final String ADMIN_AUTHORIZATION = "Authorization (ADMIN LEVEL NEEDED)";
+
     @Bean
     public OpenAPI buildAPI() {
 
@@ -26,19 +37,56 @@ public class SwaggerConfig {
                 );
     }
 
-//    @Bean
-//    public OpenApiCustomizer custumizer() {
-//
-//        return null;
-//    }
+    @Bean
+    public OpenApiCustomizer customOpenApi() {
 
-    private Parameter getAdminToken() {
-
-        return null;
+        return openApi -> openApi
+                .getPaths()
+                .forEach(this::getRequests);
     }
 
-    private Parameter getUserToken() {
+    private void getRequests(String path, PathItem pathItem) {
 
-        return null;
+        if (nonNull(pathItem.getGet())) {
+            pathItem.getGet().addParametersItem(createUserTokenParameter());
+        }
+
+        if (nonNull(pathItem.getPost()) && !path.toLowerCase().contains("/token")) {
+            pathItem.getPost().addParametersItem(createAdminTokenParameter());
+        }
+
+        if (nonNull(pathItem.getPut())) {
+            pathItem.getPut().addParametersItem(createAdminTokenParameter());
+        }
+
+        if (nonNull(pathItem.getPatch())) {
+            pathItem.getPatch().addParametersItem(createAdminTokenParameter());
+        }
+
+        if (nonNull(pathItem.getDelete())) {
+            pathItem.getDelete().addParametersItem(createAdminTokenParameter());
+        }
+
     }
+
+    private Parameter createUserTokenParameter() {
+
+        return new Parameter()
+                .in(HEADER.toString())
+                .schema(new StringSchema())
+                .name(API_TOKEN)
+                .description(USER_AUTHORIZATION)
+                .required(TRUE);
+    }
+
+    private Parameter createAdminTokenParameter() {
+
+        return new Parameter()
+                .in(HEADER.toString())
+                .schema(new StringSchema())
+                .name(API_TOKEN)
+                .description(ADMIN_AUTHORIZATION)
+                .required(TRUE);
+    }
+
 }
