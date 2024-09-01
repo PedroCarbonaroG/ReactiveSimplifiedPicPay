8b9ebb971955855be7f1d24b1063c43441882a49
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-09-01 20:27:19 -0300

    Feature | Inclusion of some RouteDescription's and AsQueryParam's

--- src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/api/IPersonAPI.java
+++ src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/api/IPersonAPI.java
@@ -2,17 +2,12 @@ package com.carbonaro.ReactiveSimplifiedPicPay.api;
 
 import com.carbonaro.ReactiveSimplifiedPicPay.core.annotations.RouteDescriptions.*;
 import com.carbonaro.ReactiveSimplifiedPicPay.core.annotations.RouteParams.LegalPersonRequestAsQueryParam;
-import com.carbonaro.ReactiveSimplifiedPicPay.core.exception_handler.response.ErrorEmptyResponse;
-import com.carbonaro.ReactiveSimplifiedPicPay.core.exception_handler.response.ErrorResponse;
+import com.carbonaro.ReactiveSimplifiedPicPay.core.annotations.RouteParams.NaturalPersonRequestAsQueryParam;
 import com.carbonaro.ReactiveSimplifiedPicPay.domain.requests.LegalPersonRequest;
 import com.carbonaro.ReactiveSimplifiedPicPay.domain.requests.NaturalPersonRequest;
 import com.carbonaro.ReactiveSimplifiedPicPay.domain.responses.person.LegalPersonResponse;
 import com.carbonaro.ReactiveSimplifiedPicPay.domain.responses.person.NaturalPersonResponse;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -27,11 +22,11 @@ public interface IPersonAPI {
     @FindAllLegalsRouteDescription
     Flux<LegalPersonResponse> findAllLegals();
 
-    @GetMapping("/legals/{cnpj}")
+    @GetMapping("/legal/{cnpj}")
     @FindLegalByCNPJRouteDescription
-    Mono<LegalPersonResponse> findLegalByCNPJ(@PathVariable String companyCNPJ);
+    Mono<LegalPersonResponse> findLegalByCNPJ(@Parameter String companyCNPJ);
 
-    @GetMapping("/legals/id")
+    @GetMapping("/legal/id")
     @FindLegalByIdRouteDescription
     Mono<LegalPersonResponse> findLegalById(@Parameter String id);
 
@@ -44,16 +39,16 @@ public interface IPersonAPI {
     @SavePartnerToLegalPersonRouteDescription
     Mono<Void> savePartnerToLegalPerson(@RequestParam String companyCNPJ, @RequestParam String partnerCPF);
 
-    @PatchMapping("/legal")
+    @PatchMapping("/legal/update")
     @LegalPersonRequestAsQueryParam
     @UpdateLegalPersonRouteDescription
     Mono<Void> updateLegalPerson(@RequestParam String companyCNPJ, @Parameter(hidden = true) LegalPersonRequest legalPerson);
 
-    @DeleteMapping("/legal")
+    @DeleteMapping("/legal/delete")
     @DeleteLegalPersonRouteDescription
-    Mono<Void> deleteLegalPerson(@RequestParam String legalPersonCNPJ);
+    Mono<Void> deleteLegalPerson(@RequestParam String companyCNPJ);
 
-    @DeleteMapping("/legal/partner")
+    @DeleteMapping("/legal/partner/delete")
     @DeletePartnerByLegalPersonRouteDescription
     Mono<Void> deletePartnerByLegalPerson(@RequestParam String companyCNPJ, @RequestParam String partnerCPF);
 
@@ -61,66 +56,26 @@ public interface IPersonAPI {
     @FindAllNaturalsRouteDescription
     Flux<NaturalPersonResponse> findAllNaturals();
 
-    @GetMapping("/naturals/{cpf}")
+    @GetMapping("/natural/{cpf}")
     @FindNaturalByCPFRouteDescription
     Mono<NaturalPersonResponse> findNaturalByCPF(@PathVariable String cpf);
 
-    @Operation(
-            summary = "Responsible route for return NaturalPerson by your own Identifier(ID).",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "OK",
-                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = LegalPersonResponse.class))),
-
-                    @ApiResponse(
-                            responseCode = "204",
-                            description = "Neither NaturalPerson was found.",
-                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorEmptyResponse.class))),
-
-                    @ApiResponse(
-                            responseCode = "400",
-                            description = "Some(s) parameter(s) could not match, revise that and try again with right parameters!",
-                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorResponse.class))),
-
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "Not Found. Resources are not found to complete the service provided by this endpoint.",
-                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorResponse.class))),
-
-                    @ApiResponse(
-                            responseCode = "500",
-                            description = "Internal Server Error. Something went wrong with API, contact the administration.",
-                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorResponse.class)))})
-    @GetMapping(value = "/naturals/id")
+    @GetMapping("/natural/id")
+    @FindNaturalByIDRouteDescription
     Mono<NaturalPersonResponse> findNaturalByID(@Parameter String id);
 
-    @Operation
     @PostMapping("/natural/save")
-    Mono<Void> saveNaturalPerson(@RequestBody NaturalPersonRequest naturalPerson);
+    @NaturalPersonRequestAsQueryParam
+    @SaveNaturalPersonRouteDescription
+    Mono<Void> saveNaturalPerson(@Parameter(hidden = true) NaturalPersonRequest naturalPerson);
 
-    @Operation(
-            summary = "Responsible route to update an NaturalPerson.",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "204",
-                            description = "Neither LegalPerson was found.",
-                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorEmptyResponse.class))),
+    @PutMapping("/natural/update")
+    @NaturalPersonRequestAsQueryParam
+    @UpdateNaturalPersonRouteDescription
+    Mono<Void> updateNaturalPerson(@Parameter String cpf, @Parameter(hidden = true) NaturalPersonRequest naturalPerson);
 
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "Not Found. Resources are not found to complete the service provided by this endpoint.",
-                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorResponse.class))),
-
-                    @ApiResponse(
-                            responseCode = "500",
-                            description = "Internal Server Error. Something went wrong with API, contact the administration.",
-                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorResponse.class)))})
-    @PutMapping("/natural")
-    Mono<Void> updateNaturalPerson(@RequestBody NaturalPersonRequest naturalPerson, @Parameter String cpf);
-
-    @Operation
-    @DeleteMapping("/natural")
-    Mono<Void> deleteNaturalPerson(@RequestBody NaturalPersonRequest naturalPerson);
+    @DeleteMapping("/natural/delete")
+    @DeleteNaturalPersonRouteDescription
+    Mono<Void> deleteNaturalPerson(@Parameter String cpf);
 
 }
