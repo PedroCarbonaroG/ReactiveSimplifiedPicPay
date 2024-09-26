e332f4d65c714724732ebb8b22d2eb3098e89798
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-09-26 19:34:02 -0300

    Feature | ApiExceptionHandler adjustments and validation of Interceptor exceptions

--- src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/api/exception_handler/ApiExceptionHandler.java
+++ src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/api/exception_handler/ApiExceptionHandler.java
@@ -6,24 +6,28 @@ import com.carbonaro.ReactiveSimplifiedPicPay.api.exception_handler.response.Err
 import com.carbonaro.ReactiveSimplifiedPicPay.api.exception_handler.response.ErrorResponse;
 import com.carbonaro.ReactiveSimplifiedPicPay.services.exceptions.*;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import io.swagger.v3.core.util.Json;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebExceptionHandler;
 import reactor.core.publisher.Mono;
 import java.nio.charset.StandardCharsets;
-import java.security.SignatureException;
+import java.util.Objects;
+
+import static com.carbonaro.ReactiveSimplifiedPicPay.AppConstants.*;
 
 @Slf4j
 @RestControllerAdvice
 @RequiredArgsConstructor
-public class ApiExceptionHandler extends ApiExceptionsHandlerHelper implements WebExceptionHandler {
+public class ApiExceptionHandler extends ApiExceptionsHandlerHelper implements ErrorWebExceptionHandler {
 
     private final MessageHelper messageHelper;
 
@@ -33,17 +37,23 @@ public class ApiExceptionHandler extends ApiExceptionsHandlerHelper implements W
 
         if (e instanceof EmptyException) {
 
-            log.warn("WARN           ====> {}", e.getMessage());
+            log.warn("WARN           ====> {}", messageHelper.getMessage(e.getMessage()));
             log.warn("WARN MESSAGE   ====> {}", e.getLocalizedMessage());
             log.warn("WARN CLASS     ====> {}", e.getClass());
             log.warn("STACKTRACE     ====> {}", (Object) e.getStackTrace());
         } else {
 
-            log.error("ERROR         ====> {}", e.getMessage());
+            log.error("ERROR         ====> {}", messageHelper.getMessage(e.getMessage()));
             log.error("ERROR MESSAGE ====> {}", e.getLocalizedMessage());
             log.error("ERROR CLASS   ====> {}", e.getClass());
             log.error("STACKTRACE    ====> {}", (Object) e.getStackTrace());
         }
+    }
+    private void getPrivateInterceptorStackTrace(Exception e) {
+
+        log.error("ERROR MESSAGE  ====> {}", e.getMessage());
+        log.error("ERROR CLASS    ====> {}", e.getClass());
+        log.error("STACKTRACE     ====> {}", (Object) e.getStackTrace());
     }
 
     @ExceptionHandler({EmptyException.class})
@@ -109,29 +119,47 @@ public class ApiExceptionHandler extends ApiExceptionsHandlerHelper implements W
     @Override
     public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
 
-        HttpStatus status = HttpStatus.UNAUTHORIZED;
-        String message = UNAUTHORIZED_ERROR_MESSAGE;
+        byte[] responseBytes = buildExceptionResponse(exchange, ex);
+        return exchange
+                .getResponse()
+                .writeWith(Mono.just(exchange.getResponse().bufferFactory().wrap(responseBytes)));
+    }
+    private byte[] buildExceptionResponse(ServerWebExchange exchange, Throwable exception) {
 
-        if (ex instanceof ExpiredJwtException) {
-            message = "ExpiredJwtException";
-        } else if (ex instanceof SignatureException) {
-            message = "SignatureException";
+        HttpStatus status = UNAUTHORIZED_STATUS;
+        String error = UNAUTHORIZED;
+        String message = validateException(exception);
+
+        if (Objects.isNull(message)) {
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+            error = INTERNAL_SERVER_ERROR;
+            message = INTERNAL_SERVER_ERROR_MESSAGE;
         }
 
+        getPrivateInterceptorStackTrace((Exception) exception);
         ErrorResponse response = ErrorResponse
                 .builder()
-                .error(UNAUTHORIZED)
+                .error(error)
                 .timestamp(TIMESTEMP)
                 .path(getPath(exchange))
-                .status(UNAUTHORIZED_STATUS.value())
+                .status(status.value())
                 .errorMessage(message)
                 .build();
 
         exchange.getResponse().setStatusCode(status);
         exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
-        byte[] responseBytes = Json.pretty(response).getBytes(StandardCharsets.UTF_8);
 
-        return exchange.getResponse().writeWith(Mono.just(exchange.getResponse().bufferFactory().wrap(responseBytes)));
+        return Json.pretty(response).getBytes(StandardCharsets.UTF_8);
+    }
+    private String validateException(Throwable ex) {
+
+        String defaultMessage = UNAUTHORIZED_ERROR_MESSAGE;
+        if (ex instanceof ExpiredJwtException) return messageHelper.getMessage(TOKEN_EXPIRED_ERROR).concat(SEPARATOR).concat(defaultMessage);
+        else if (ex instanceof SignatureException) return messageHelper.getMessage(TOKEN_SIGNATURE_ERROR).concat(SEPARATOR).concat(defaultMessage);
+        else if (ex instanceof MalformedJwtException) return messageHelper.getMessage(TOKEN_STRUCTURE_ERROR).concat(SEPARATOR).concat(defaultMessage);
+        else if (ex instanceof IllegalArgumentException) return messageHelper.getMessage(TOKEN_SCOPES_ERROR).concat(SEPARATOR).concat(defaultMessage);
+        else return null;
+
     }
 
 }
