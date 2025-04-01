213ade7ffbc4af6c4ed3fb00295b0bc4510c5f67
Pedro Carbonaro <124113283+PedroCarbonaroG@users.noreply.github.com>
2025-04-01 16:23:53 -0300

    Update ExportingImpl.java

--- src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/api/impl/ExportingImpl.java
+++ src/main/java/com/carbonaro/ReactiveSimplifiedPicPay/api/impl/ExportingImpl.java
@@ -1,13 +1,17 @@
 package com.carbonaro.ReactiveSimplifiedPicPay.api.impl;
 
 import com.carbonaro.ReactiveSimplifiedPicPay.api.IExportingAPI;
+import com.carbonaro.ReactiveSimplifiedPicPay.api.requests.person.LegalPersonFilterRequest;
+import com.carbonaro.ReactiveSimplifiedPicPay.api.requests.person.NaturalPersonFilterRequest;
 import com.carbonaro.ReactiveSimplifiedPicPay.api.requests.transaction.TransactionFilterRequest;
-import com.carbonaro.ReactiveSimplifiedPicPay.domain.entities.Person;
+import com.carbonaro.ReactiveSimplifiedPicPay.domain.entities.LegalPerson;
+import com.carbonaro.ReactiveSimplifiedPicPay.domain.entities.NaturalPerson;
 import com.carbonaro.ReactiveSimplifiedPicPay.domain.entities.Transaction;
 import com.carbonaro.ReactiveSimplifiedPicPay.domain.enums.FileTypeEnum;
 import com.carbonaro.ReactiveSimplifiedPicPay.services.ExportingService;
 import com.carbonaro.ReactiveSimplifiedPicPay.services.helper.ExportingServiceHelper;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +19,7 @@ import reactor.core.publisher.Mono;
 
 import static com.carbonaro.ReactiveSimplifiedPicPay.AppConstants.ADMIN_SCOPE;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 public class ExportingImpl implements IExportingAPI {
@@ -23,18 +28,29 @@ public class ExportingImpl implements IExportingAPI {
 
     @Override
     @PreAuthorize(value = ADMIN_SCOPE)
-    public Mono<ResponseEntity<byte[]>> getTransactionsExtraction(TransactionFilterRequest filterRequest, FileTypeEnum fileType) {
+    public Mono<ResponseEntity<byte[]>> transactionsToExtraction(TransactionFilterRequest filter, FileTypeEnum fileType) {
 
-        return exportingService.getTransactionsExtraction(filterRequest, fileType)
+        log.info("Exporting transactions to extraction report for {} format", fileType);
+        return exportingService.getTransactionsExtraction(filter, fileType)
                 .flatMap(bytesToExport -> ExportingServiceHelper.exportingResponse(bytesToExport, fileType, Transaction.class));
     }
 
     @Override
     @PreAuthorize(value = ADMIN_SCOPE)
-    public Mono<ResponseEntity<byte[]>> getPersonsToExtraction(FileTypeEnum fileType) {
+    public Mono<ResponseEntity<byte[]>> naturalsToExtraction(NaturalPersonFilterRequest filter, FileTypeEnum fileType) {
 
-        return exportingService.getPersonsToExtraction(fileType)
-                .flatMap(bytesToExport -> ExportingServiceHelper.exportingResponse(bytesToExport, fileType, Person.class));
+        log.info("Exporting naturals to extraction report for {} format", fileType);
+        return exportingService.getNaturalsToExtraction(filter, fileType)
+                .flatMap(bytesToExport -> ExportingServiceHelper.exportingResponse(bytesToExport, fileType, NaturalPerson.class));
+    }
+
+    @Override
+    @PreAuthorize(value = ADMIN_SCOPE)
+    public Mono<ResponseEntity<byte[]>> legalsToExtraction(LegalPersonFilterRequest filter, FileTypeEnum fileType) {
+
+        log.info("Exporting legals to extraction report for {} format", fileType);
+        return exportingService.getLegalsToExtraction(filter, fileType)
+                .flatMap(bytesToExport -> ExportingServiceHelper.exportingResponse(bytesToExport, fileType, LegalPerson.class));
     }
 
 }
